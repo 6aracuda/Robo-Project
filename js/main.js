5f369c56051bb49cf12594c7a0616cfde8258d10
sshurkhovetskii <sshurkhovetskii@gmail.com>
2023-12-06 13:35:32 +0200

    fix Some Functional

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const parent = document.querySelector('.icon__i')
+// let parent = document.querySelector('.icon__i')
 
-function show(){
-    document.querySelector('.hint').classList.add('show')
+// function show() {
+//    document.querySelector('.hint').classList.add('show')
+// }
+// function hidden() {
+//    document.querySelector('.hint').classList.remove('show')
+// }
+// parent.addEventListener('mouseover', show)
+// parent.addEventListener('mouseout', hidden)
+
+function MouseOver() {
+   document.addEventListener('DOMContentLoaded', function () {
+      let card = document.querySelector('.icon__i');
+      console.log(card);
+      card.addEventListener('mouseover', function () {
+         let hint = document.querySelector('.hint');
+         hint.classList.toggle('show');
+      });
+   });
 }
-function hidden(){
-    document.querySelector('.hint').classList.remove('show')
-}
-parent.addEventListener('mouseover', show)
-parent.addEventListener('mouseout', hidden)
+
+MouseOver();
